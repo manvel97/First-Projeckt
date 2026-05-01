bfb7bf6d164afd695c00111ab27f56ecf29f4017
manvel97 <133569676+manvel97@users.noreply.github.com>
2026-05-01 14:08:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Barev Yurik Jan Dzya")
+let Ուտաչնիտ;
+document.getElementById("mySubmit"). onclick = function (){
+Ուտաչնիտ = document.getElementById("myText").value;
+console.log(Ուտաչնիտ);
+}
